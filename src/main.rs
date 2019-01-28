b09ce9ff4d624f0bbbcb64763b77a329603d3051
jundl77 <julianbrendl@gmail.com>
2019-01-28 21:31:51 +0100

    added cors and logging

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,40 @@ extern crate router;
 extern crate bodyparser;
 extern crate persistent;
 extern crate serde_json;
+extern crate iron_cors;
+extern crate logger;
+extern crate simplelog;
 
-use persistent::Read;
+#[macro_use]
+extern crate log;
+
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+use iron_cors::CorsMiddleware;
+use logger::Logger;
+use simplelog::*;
 
 mod transpile;
 
 fn main() {
+    TermLogger::init(LevelFilter::Info, Config::default());
+
+    let (logger_before, logger_after) = Logger::new(None);
+    let cors_middleware = CorsMiddleware::with_allow_any();
+
     let mut router = Router::new();
     router.get("/status", status_handler, "status");
     router.post("/transpile", transpile_handler, "transpile");
 
+    let mut chain = Chain::new(router);
+    chain.link_before(logger_before);
+    chain.link_around(cors_middleware);
+    chain.link_after(logger_after);
+
     let address = "localhost:3001";
-    println!("Iron listening on {}", address);
-    Iron::new(router).http(address).unwrap();
+    info!("Iron listening on {}", address);
+    Iron::new(chain).http(address).unwrap();
 }
 
 fn status_handler(req: &mut Request) -> IronResult<Response> {
